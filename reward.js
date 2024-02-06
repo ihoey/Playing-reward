d1c8e91e66569a7329711edcf38d051da21868d5
ihoey <mail@ihoey.com>
2024-02-06 15:56:28 +0800

    style: 优化代码格式 统一名称规范

--- reward.js
+++ reward.js
@@ -1,39 +1,37 @@
-jQuery(document).ready(function () {
-	var QRBox = $('#QRBox');
-	var MainBox = $('#MainBox');
-	var QQPayQR = 'images/QQPayQR.png';
-	var AliPayQR = 'images/AliPayQR.png';
-	var WeChanQR = 'images/WeChanQR.png';
+jQuery(document).ready(() => {
+  var QRBox = $("#QRBox");
+  var MainBox = $("#MainBox");
+  var QQPayQR = "images/QQPayQR.png";
+  var AliPayQR = "images/AliPayQR.png";
+  var WeChanQR = "images/WeChanQR.png";
 
-	function showQR(QR) {
-		if (QR) {
-			MainBox.css('background-image', 'url(' + QR + ')');
-		}
-		$('#DonateText,#donateBox,#github').addClass('blur');
-		QRBox.fadeIn(300, function (argument) {
-			MainBox.addClass('showQR');
-		});
-	}
+  const showQR = (QR) => {
+    if (QR) MainBox.css("background-image", `url(${QR})`);
 
-	$('#donateBox>li').click(function (event) {
-		var thisID = $(this).attr('id');
-		if (thisID === 'QQPay') {
-			showQR(QQPayQR);
-		} else if (thisID === 'AliPay') {
-			showQR(AliPayQR);
-		} else if (thisID === 'WeChat') {
-			showQR(WeChanQR);
-		}
-	});
+    $("#DonateText,#donateBox,#github").addClass("blur");
+    QRBox.fadeIn(300, () => {
+      MainBox.addClass("showQR");
+    });
+  };
 
-	MainBox.click(function (event) {
-		MainBox.removeClass('showQR').addClass('hideQR');
-		setTimeout(function (a) {
-			QRBox.fadeOut(300, function (argument) {
-				MainBox.removeClass('hideQR');
-			});
-			$('#DonateText,#donateBox,#github').removeClass('blur');
-		}, 600);
+  $("#donateBox>li").click((event) => {
+    var thisID = $(this).attr("id");
+    if (thisID === "QQPay") {
+      showQR(QQPayQR);
+    } else if (thisID === "AliPay") {
+      showQR(AliPayQR);
+    } else if (thisID === "WeChat") {
+      showQR(WeChanQR);
+    }
+  });
 
-	});
+  MainBox.click((event) => {
+    MainBox.removeClass("showQR").addClass("hideQR");
+    setTimeout((a) => {
+      QRBox.fadeOut(300, () => {
+        MainBox.removeClass("hideQR");
+      });
+      $("#DonateText,#donateBox,#github").removeClass("blur");
+    }, 600);
+  });
 });
