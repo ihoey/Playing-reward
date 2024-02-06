13a69d8ab822c1dd8b4031aef83bfa87fbe13a5d
ihoey <mail@ihoey.com>
2024-02-06 15:44:31 +0800

    style: 优化代码格式 统一名称规范

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Playing-reward
+
 一个打赏的小功能
 
 修改自 [donate-page](https://github.com/Kaiyuan/donate-page)
@@ -9,7 +10,7 @@
 [![](https://img.shields.io/badge/QQ-✘д✘ヽゝだよ-red.svg?style=flat-square)](http://wpa.qq.com/msgrd?v=3&uin=1058221214&site=qq&menu=yes)
 [![](https://img.shields.io/badge/blog-ihoey-ff69b4.svg?style=flat-square)](https://blog.ihoey.com)
 
-博客版本请查看 `hexo` 分支  [点击跳转](https://github.com/ihoey/Playing-reward/tree/hexo)
+博客版本请查看 `hexo` 分支 [点击跳转](https://github.com/ihoey/Playing-reward/tree/hexo)
 
 ### 直接 `Fork`
 
@@ -20,7 +21,12 @@
 ### 使用 iframe 嵌入页面的代码，高度至少 240px，宽度至少 310px！
 
 ```html
-<iframe src="https://ihoey.github.io/Playing-reward/" style="overflow-x:hidden;overflow-y:hidden; border:0xp none #fff; min-height:240px; width:100%;"  frameborder="0" scrolling="no"></iframe>
+<iframe
+  src="https://ihoey.github.io/Playing-reward/"
+  style="overflow-x:hidden;overflow-y:hidden; border:0xp none #fff; min-height:240px; width:100%;"
+  frameborder="0"
+  scrolling="no"
+></iframe>
 ```
 
 如果觉得还可以的话，点个赞呗~
@@ -28,6 +34,5 @@
 打赏一下的话就更好了~
 
 ![微信支付](images/WeChanQR.png)
-
 
 ![支付宝支付](images/AliPayQR.png)
--- script.js
+++ script.js
@@ -1,32 +1,36 @@
-const bd = document.querySelector('body')
-const qbox = bd.querySelector('#QRBox')
-const mbox = bd.querySelector('#MainBox')
-const dbox = bd.querySelector('#donateBox')
+const bd = document.querySelector("body");
 
-const qqr = 'images/QQPayQR.png'
-const aqr = 'images/AliPayQR.png'
-const wqr = 'images/WeChanQR.png'
+const QRBox = bd.querySelector("#QRBox");
+const MainBox = bd.querySelector("#MainBox");
+const donateBox = bd.querySelector("#donateBox");
+
+const qqr = "images/QQPayQR.png";
+const aqr = "images/AliPayQR.png";
+const wqr = "images/WeChanQR.png";
 
 const showQR = (QR) => {
-	if (QR) mbox.style.backgroundImage = `url(${ QR })`
-	bd.classList.add('blur')
-	qbox.classList.add('fadeIn')
-	mbox.classList.add('showQR')
-}
+  if (QR) MainBox.style.backgroundImage = `url(${QR})`;
 
-dbox.addEventListener('click', (e) => {
-	let el = e.target
-	if (el.id === 'QQPay') showQR(qqr)
-	else if (el.id === 'AliPay') showQR(aqr)
-	else if (el.id === 'WeChat') showQR(wqr)
-})
+  bd.classList.add("blur");
+  QRBox.classList.add("fadeIn");
+  MainBox.classList.add("showQR");
+};
 
-mbox.addEventListener('click', () => {
-	mbox.classList.remove('showQR')
-	mbox.classList.add('hideQR')
-	setTimeout(a => {
-		qbox.classList.remove('fadeIn')
-		mbox.classList.remove('hideQR')
-		bd.classList.remove('blur')
-	}, 600)
-})
+donateBox.addEventListener("click", (e) => {
+  let el = e.target;
+
+  if (el.id === "QQPay") showQR(qqr);
+  else if (el.id === "AliPay") showQR(aqr);
+  else if (el.id === "WeChat") showQR(wqr);
+});
+
+MainBox.addEventListener("click", () => {
+  MainBox.classList.remove("showQR");
+  MainBox.classList.add("hideQR");
+
+  setTimeout((a) => {
+    QRBox.classList.remove("fadeIn");
+    MainBox.classList.remove("hideQR");
+    bd.classList.remove("blur");
+  }, 600);
+});
